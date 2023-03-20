780b7e6aeea9e7825603a73456ada22b947433ee
AudreyFelicio <audreyanwar@gmail.com>
2023-03-20 12:29:25 +0800

    feat: Add generator for dataset

---
+++ generator.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <iostream>
+
+auto fast_io() -> void {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+}
+
+auto main() -> int {
+  int n, k;
+  std::cin >> n >> k;
+
+  std::vector<int> permutation(n, 0);
+  std::iota(permutation.begin(), permutation.end(), 1);
+  std::random_device rd;
+  std::mt19937 g(rd());
+
+  for (auto i = 0; i < k; ++i) {
+    shuffle(permutation.begin(), permutation.end(), g);
+    for (auto p : permutation) {
+      std::cout << p << " ";
+    }
+    std::cout << "\n";
+  }
+  std::cout.flush();
+}
